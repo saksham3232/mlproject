38dcfb976e0b897774608f559eb258d9065eb6c2
Saksham Maurya <sakshammaurya3232@gmail.com>
2025-04-16 16:42:45 +0530

    pipeline setup

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -51,5 +52,16 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
         return report
 
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    """
+    Load the object from a file using pickle.
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
